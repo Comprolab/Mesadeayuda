39b83f688c2d036fa72ec77008e5f2511a22bdfd
jamestesubelanota <64816996+jamestesubelanota@users.noreply.github.com>
2021-09-28 08:07:31 -0500

    creacion y persitencia de datos maestros para reporte de servicio

--- admin/reportes.php
+++ admin/reportes.php
@@ -47,51 +47,49 @@ require_once(HESK_PATH . 'inc/show_admin_nav.inc.php');
     <script src="../js/jquery.dataTables.min.js"></script>
 
 </head>
+
+
 <?php
 
-// agregar
 
-$id = $_POST['id'];
-$nombre = $_POST['nombre'];
-$ingeniero = $_POST['ingeniero'];
-
-
-// al presionar en botón de crear
-if (isset($_POST['Crear'])) {
-    // condición para confirmar que no estén vacios los campos
-    // if (isset($_POST['id']) && isset($_POST['nombre'])) {
-
-        // condicion para saber si hay un ingeniero al que se le vaya a asignar la zona
-        // if (isset($_POST['ingeniero'])) {
-
-            hesk_dbQuery("INSERT INTO
+ 
+    $nombreReporte = $_POST["nombreReporte"];
+    $asignado      = $_POST["asignado"];
+    $fechaInicio   = $_POST["fechaInicio"];
+    $fechaFinal    = $_POST["fechaFinal"];
+    $estado        = $_POST["estado"];
+    $localizacion  = $_POST["localizacion"];
+    $descripcion   = $_POST["descripcion"];
+    $estadoEquipo  = $_POST["estadoEquipo"];
+    $recomendacion = $_POST["recomendacion"];
+    $formaPago     = $_POST["formaPago"];
+    $profecional   = $_POST["profecional"];
+    $cargo         = $_POST["cargo"];
+    $nombreCliente = $_POST["nombreCliente"];
+    $direccion     = $_POST["direccion"];
+    $numero        = $_POST["numero"];
+     
+    #$query = "INSERT INTO hesk_reporte_de_servicio (nombreReporte, asignado, fechaInicio, fechaFinal, 
+     #estado, localizacion, descripcion , estadoEquipo, recomendacion, formaPago, profecional, cargo, nombreCliente, direccion, numero ) VALUES('$nombreReporte', '$asignado', '$fechaInicio', '$fechaFinal', '$estado', $localizacion, '$descripcion',
+     #'$estadoEquipo', '$recomendacion', '$formaPago', $profecional, '$cargo', $nombreCliente, '$direccion', $numero )";
     
-            hesk_zones (
-                nombre
-            )
-            VALUES (
-                '$nombre'
-            )
-            
-            ");
-        // } else {
-        //     hesk_dbQuery("INSERT INTO
+
+    $query2 = "INSERT INTO `hesk_reporte_de_servicio` ( `nombreReporte`, `asignado`, `fechaInicio`, `fechaFinal`, `estado`, `localizacion`, `descripcion`, `estadoEquipo`, `recomendacion`, `formaPago`, `profecional`, `cargo`, `nombreCliente`, `direccion`, `numero`) 
+    VALUES ( '$nombreReporte', '$asignado', '$fechaInicio', '$fechaFinal', '$estado', '$localizacion', '$descripcion', '$estadoEquipo', '$recomendacion', '$formaPago', '$profecional', '$cargo', '$nombreCliente', '$direccion', '$numero');";
+     echo $query;
+     hesk_dbQuery($query2);
+
+     
+     
     
-        //     hesk_zones (
-        //         codigo_zona,
-        //         nombre
-        //     )
-        //     VALUES (
-        //         '$id',
-        //         '$nombre'
-        //     )
-            
-        //     ");
-        }
-    // } else {
-    //     echo "Por favor ingrese todos los datos";
-    // }
-// }
+
+   
+
+
+
+
+
+
 
 
 ?>
@@ -101,19 +99,15 @@ if (isset($_POST['Crear'])) {
     <h1 class="h1est">Agregar</h1>
     <div  class="table-wrap">
 
-        <form  action="add_zone.php" method="post" class="form <?php echo isset($_SESSION['iserror']) && count($_SESSION['iserror']) ? 'invalid' : ''; ?>" onsubmit='return validar()'>
+        <form  action="reportes.php" method="post" class="form <?php echo isset($_SESSION['iserror']) && count($_SESSION['iserror']) ? 'invalid' : ''; ?>" onsubmit='return validar()'>
 
         <div class="form-group">
                     <label>
                         <span>Tipo de reporte </span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                               
-                            </div>
-                        </a>
+                       
                         <div class="form-group">
                 <select name="nombreReporte" id="nombreReporte" class="form-control" style="border: 90%;">}
-                <option value="value1">Reporte de servicio</option>
+                <option value="Reporte de servicio">Reporte de servicio</option>
              
                 </select>
                     </label>
@@ -123,13 +117,7 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Asignado </span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                <svg class="icon icon-info">
-                                    <use xlink:href="<?php echo HESK_PATH; ?>img/sprite.svg#icon-info"></use>
-                                </svg>
-                            </div>
-                        </a>
+                 
                     </label>
                     <select name="asignado" id="asignado" class="form-control" style="border: 90%;">}
        
@@ -149,55 +137,39 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Fecha de inicio </span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                <svg class="icon icon-info">
-                                    <use xlink:href="<?php echo HESK_PATH; ?>img/sprite.svg#icon-info"></use>
-                                </svg>
-                            </div>
-                        </a>
+                    
                     </label>
+                
                     <input id="fechaInicio" type="date" class="form-control" name="fechaInicio" maxlength="255" >
-                </div>
+             </div>
 
                 <div class="form-group">
                     <label>
                         <span>Fecha final </span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                
-                            </div>
-                        </a>
+                     
                     </label>
-                    <input type="date" class="form-control" name="fechaFinal" maxlength="255" value="">
-                </div>
+                  
+                    <input type="date" class="form-control" name="fechaFinal" maxlength="255" >
+                    </div>
 
                 <div class="form-group">
                     <label>
                         <span>Estado de servicio </span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-
-                            </div>
-                        </a>
+         
                     </label>
                     <select name="estado" id="estado" class="form-control" style="border: 90%;">}
-                <option value="value1">Realizado</option>
-                <option value="value1">Pendiente </option>
-                <option value="value1">Aplazado </option>
-                <option value="value1">En operacion </option>    
-                <option value="value1">Planeado</option>
+                <option value="Realizado">Realizado</option>
+                <option value="Pendiente">Pendiente </option>
+                <option value="Aplazado">Aplazado </option>
+                <option value="En operacion">En operacion </option>    
+                <option value="Planeado">Planeado</option>
                 </select>
                 </div>
 
                 <div class="form-group">
                     <label>
                         <span>Localizacion</span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                               
-                            </div>
-                        </a>
+                    
                     </label>
                     <select name="localizacion" id="localizacion" class="form-control">
                     <?php 
@@ -215,11 +187,7 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Descripcion de servicio</span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                              
-                            </div>
-                        </a>
+                 
                     </label>
                    
                     <textarea id="descripcion" style="width: 100%;" type="textarea" class="form-control" name="descripcion" maxlength="255" value="" ></textarea>
@@ -229,11 +197,7 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Estado de equipo</span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                               
-                            </div>
-                        </a>
+               
                     </label>
                     <textarea style="width: 100%;" type="textarea" class="form-control" id="estadoEquipo" name="estadoEquipo" maxlength="255" value="" ></textarea>
                 </div>
@@ -242,13 +206,7 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Recomendacion</span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                <svg class="icon icon-info">
-                                    <use xlink:href="<?php echo HESK_PATH; ?>img/sprite.svg#icon-info"></use>
-                                </svg>
-                            </div>
-                        </a>
+                  
                     </label>
                     <textarea style="width: 100%;" type="textarea" class="form-control" id="recomendacion" name="recomendacion" maxlength="255" value="" ></textarea>
                 </div>
@@ -257,18 +215,14 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Forma de pago </span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                               
-                            </div>
-                        </a>
+                    
                     </label>
                     <select id="formaPago" name="formaPago" id="formaPago" class="form-control" style="border: 90%;">
-                    <option value="value1"></option>
-                <option value="value1">Comodato</option>
-                <option value="value1">Credito</option>
-                <option value="value1">Efectivo </option>
-                <option value="value1">Devito </option>    
+                <option value=""></option>
+                <option value="Comodato">Comodato</option>
+                <option value="Credito">Credito</option>
+                <option value="Efectivo">Efectivo </option>
+                <option value="Devito">Devito </option>    
           
                 </select>
                 </div>
@@ -277,13 +231,7 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Profecional</span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                <svg class="icon icon-info">
-                                    <use xlink:href="<?php echo HESK_PATH; ?>img/sprite.svg#icon-info"></use>
-                                </svg>
-                            </div>
-                        </a>
+                      
                     </label>
                     <select id="profecional" name="profecional" id="zona" class="form-control" style="border: 90%;">}
        
@@ -303,21 +251,15 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Cargo</span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                <svg class="icon icon-info">
-                                    <use xlink:href="<?php echo HESK_PATH; ?>img/sprite.svg#icon-info"></use>
-                                </svg>
-                            </div>
-                        </a>
+                    
                     </label>
                     <select name="cargo" id="cargo" class="form-control" style="border: 90%;">
                     <option value="value1"></option>
-                <option value="value1">Ingeniero de sistemas </option>
-                <option value="value1">Tecnico de servicio</option>
-                <option value="value1">Ingeniero electrico </option>
-                <option value="value1">Ingeniero de zona  </option>    
-                <option value="value1">Bacteriologo </option> 
+                <option value="Ingeniero de sistemas ">Ingeniero de sistemas </option>
+                <option value="Tecnico de servicio">Tecnico de servicio</option>
+                <option value="Ingeniero electrico">Ingeniero electrico </option>
+                <option value="Ingeniero de zona ">Ingeniero de zona  </option>    
+                <option value="Bacteriologo ">Bacteriologo </option> 
           
                 </select>
                 </div>
@@ -327,16 +269,10 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Nombre del cliente </span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                <svg class="icon icon-info">
-                                    <use xlink:href="<?php echo HESK_PATH; ?>img/sprite.svg#icon-info"></use>
-                                </svg>
-                            </div>
-                        </a>
+                  
                     </label>
                     <select name="nombreCliente" id="nombreCliente" class="form-control">
-                    <option disabled selected value="0">Cliente</option>
+                    <option  value="Cliente">Cliente</option>
                     <?php
                     $sql = "SELECT id,nombre FROM hesk_customers";
                     $res = hesk_dbQuery($sql);
@@ -345,7 +281,7 @@ if (isset($_POST['Crear'])) {
                         <option value="<?php echo $reg['id'] ?>"><?php echo $reg['nombre'] ?></option>
                     <?php
                     }
-                    ?>
+                    ?>  
                 </select>
                 </div>
 
@@ -354,13 +290,7 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Direccion</span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                <svg class="icon icon-info">
-                                    <use xlink:href="<?php echo HESK_PATH; ?>img/sprite.svg#icon-info"></use>
-                                </svg>
-                            </div>
-                        </a>
+                     
                     </label>
                     <input type="text" class="form-control" id="direccion" name="direccion" maxlength="255" value="">
                 </div>
@@ -369,13 +299,7 @@ if (isset($_POST['Crear'])) {
                 <div class="form-group">
                     <label>
                         <span>Numero de telefono del cliente </span>
-                        <a onclick="hesk_window('<?php echo $help_folder; ?>general.html#1','400','500')">
-                            <div class="tooltype right">
-                                <svg class="icon icon-info">
-                                    <use xlink:href="<?php echo HESK_PATH; ?>img/sprite.svg#icon-info"></use>
-                                </svg>
-                            </div>
-                        </a>
+                      
                     </label>
                     <input type="text" class="form-control" id="numero" name="numero" maxlength="255" value="">
                 </div>
@@ -386,7 +310,7 @@ if (isset($_POST['Crear'])) {
                 
 
 
-            <input name="Crear " type="submit" value="Crear Reporte" class="btn btn-full">
+            <input id="Crear" name="Crear " type="submit" value="Crear Reporte" class="btn btn-full">
         </form>
     </div>
 
